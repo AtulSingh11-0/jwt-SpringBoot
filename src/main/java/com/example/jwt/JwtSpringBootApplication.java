5245e51a52f4e9c18bad7c219f2ee25b060b6fcd
Atul Singh <cocatul11@gmail.com>
2024-04-14 00:09:09 +0530

    second commit

--- src/main/java/com/example/jwt/JwtSpringBootApplication.java
+++ src/main/java/com/example/jwt/JwtSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JwtSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwtSpringBootApplication.class, args);
-	}
+  public static void main ( String[] args ) {
+    SpringApplication.run(JwtSpringBootApplication.class, args);
+  }
 
 }
